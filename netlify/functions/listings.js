ecbf19215b573fe0c582b0420b56955cc113fba2
Dante Capicchioni <dante.capicchioni@kellogg.northwestern.edu>
2021-05-06 22:03:15 -0500

    done

--- netlify/functions/listings.js
+++ netlify/functions/listings.js
@@ -14,30 +14,49 @@ let fs = require('fs')
 // defines a lambda function
 exports.handler = async function(event) {
 
+  console.log(event.queryStringParameters.bedrooms)
+
+  let minBedrooms = event.queryStringParameters.minBedrooms
+
   // read listings CSV file from disk
   let listingsFile = fs.readFileSync(`./listings.csv`)
   
   // turn the listings file into a JavaScript object, wait for that to happen
   let listingsFromCsv = await csv(listingsFile)
+  // console.log(listingsFromCsv)
 
   // write the number of listings (the array's length) to the back-end console
-
+console.log(`There are ${listingsFromCsv.length} listings`)
   // write the first few listings to the back-end console, to see what we're working with
-
+console.log(listingsFromCsv[0])
   // create a new object to hold the count and listings data
+let listingsToReturn = {
+  listings: [],
+  count: 0
 
+}
   // start with an empty Array for the listings
-  
+  let listings = []
+
   // loop through all listings, for each one:
+  for (let x = 0; x < listingsFromCsv.length; x++) {
+
     // store each listing in memory
+    let listing = listingsFromCsv[x]
+    // console.log(listing)
     // check if the rating is at least 99, if so:
+    if (listing.review_scores_rating >= 99 && listing.bedrooms >= minBedrooms) {
       // add the listing to the Array of listings to return
+    listingsToReturn.listings.push(listing)
+    // listingsToReturn.count = listingsToReturn.count + 1
+    }
+  }
 
   // add the number of listings to the returned listings Object
 
   // a lambda function returns a status code and a string of data
   return {
     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    body: `Hello from the back-end!` // a string of data
+    body: JSON.stringify(listingsToReturn) // a string of data
   }
 }
--- netlify/functions/todos.js
+++ netlify/functions/todos.js
@@ -2,16 +2,17 @@
 exports.handler = async function(event) {
 
   // an Array of to-dos
+  let todos = [`Take a walk`, `Go Workout`, `Go shopping`]
 
   // write the Array of to-dos to the *back-end* console
-
+console.log(todos)
   // turn the Array into a String
-
+let todosAsString = JSON.stringify(todos)
   // write the "stringified" Array to the *back-end* console
-
+console.log(todosAsString)
   // a lambda function returns a status code and a string of data
   return {
     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    body: `Hello from the back-end!` // a string of data
+    body: todosAsString // a string of data
   }
 }
